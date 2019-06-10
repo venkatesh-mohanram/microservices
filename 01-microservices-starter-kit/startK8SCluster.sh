b76b2e4022985b39d44478ed88731756cfc6c372
Venkatesh Mohanram <43698009+venkatesh-mohanram@users.noreply.github.com>
2019-06-10 15:37:42 +0530

    Update startK8SCluster.sh

--- 01-microservices-starter-kit/startK8SCluster.sh
+++ 01-microservices-starter-kit/startK8SCluster.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 kubectl apply -f add-service/k8s-deployment.yaml -f add-service/k8s-service.yaml -f sub-service/k8s-deployment.yaml -f sub-service/k8s-service.yaml -f calculator-service/k8s-deployment.yaml -f calculator-service/k8s-service.yaml
